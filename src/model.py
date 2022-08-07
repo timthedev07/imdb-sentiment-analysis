8d415f778acecccf9e886ad54e828df7fbbb9397
Tim <harvard90873@gmail.com>
2022-08-08 00:23:44 +0200

    complete model code

--- src/model.py
+++ src/model.py
@@ -8,26 +8,42 @@ from tensorflow.python.keras import Sequential as ST
 from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D, Dropout
 from tensorflow.keras.layers import TextVectorization
 import nltk
+import numpy as np
 from nltk.corpus import stopwords
-
-dataDir = "data-csv"
 
 pj = os.path.join
 
+SAVED_MODEL_DIR = "model"
+
 # the number of nodes in the hidden layers
-HIDDEN_SIZE_L1 = 256
-HIDDEN_SIZE_L2 = 128
 EPOCHS = 15
 VOCAB_SIZE = 10000
 SEQUENCE_LENGTH = 100
 
-def readCSV():
+def custom_standardization(input_data):
+    stop_words = set(stopwords.words('english'))
+
+    lowercase = tf.strings.lower(input_data)
+    stripped_html = tf.strings.regex_replace(lowercase, "<br />", " ")
+    stripped_html = tf.strings.regex_replace(stripped_html,r'\d+(?:\.\d*)?(?:[eE][+-]?\d+)?', ' ')
+    stripped_html = tf.strings.regex_replace(stripped_html, r'@([A-Za-z0-9_]+)', ' ' )
+    for i in stop_words:
+        stripped_html = tf.strings.regex_replace(stripped_html, f' {i} ', " ")
+    return tf.strings.regex_replace(
+        stripped_html, "[%s]" % re.escape(string.punctuation), ""
+    )
+
+
+def readCSV(dataDir = "data-csv"):
     train = pd.read_csv(pj(dataDir, "train.csv"), encoding="utf-8")
     test = pd.read_csv(pj(dataDir, "test.csv"), encoding="utf-8")
     return (train, test)
 
-def getDataset():
-    (train, test) = readCSV()
+def getDataset(dataSource="small"):
+    if dataSource == "small":
+        (train, test) = readCSV()
+    else:
+        (train, test) = readCSV("large-data-csv")
 
     trainX = train.loc[:, "review"].values
     trainY = train.loc[:, "sentiment"].values
@@ -38,25 +54,12 @@ def getDataset():
     return (trainX, trainY, testX, testY)
 
 def getTrainedModel() -> ST:
-    (trainX, trainY, testX, testY) = getDataset()
+    (trainX, trainY, testX, testY) = getDataset(dataSource="large")
+    print(testY.head)
 
     # Vocabulary size and number of words in a sequence.
 
     nltk.download('stopwords')
-
-    def custom_standardization(input_data):
-        stop_words = set(stopwords.words('english'))
-
-        lowercase = tf.strings.lower(input_data)
-        stripped_html = tf.strings.regex_replace(lowercase, "<br />", " ")
-        stripped_html = tf.strings.regex_replace(stripped_html,r'\d+(?:\.\d*)?(?:[eE][+-]?\d+)?', ' ')
-        stripped_html = tf.strings.regex_replace(stripped_html, r'@([A-Za-z0-9_]+)', ' ' )
-        for i in stop_words:
-            stripped_html = tf.strings.regex_replace(stripped_html, f' {i} ', " ")
-        return tf.strings.regex_replace(
-            stripped_html, "[%s]" % re.escape(string.punctuation), ""
-        )
-
 
     vectorize_layer = TextVectorization(
         standardize=custom_standardization,
@@ -64,16 +67,16 @@ def getTrainedModel() -> ST:
         output_mode='int',
         output_sequence_length=SEQUENCE_LENGTH)
 
-    vectorize_layer.adapt(trainX + testX)
+    vectorize_layer.adapt(np.concatenate([trainX, testX]))
 
-    embedding_dim=16
+    embedding_dim = 32
 
     model = Sequential([
         vectorize_layer,
         Embedding(VOCAB_SIZE, embedding_dim, name="embedding"),
         GlobalAveragePooling1D(),
         Dense(32, activation='relu'),
-        Dropout(0.45),
+        Dropout(0.5),
         Dense(1)
     ])
 
@@ -86,18 +89,26 @@ def getTrainedModel() -> ST:
     model.fit(trainX, trainY, epochs=EPOCHS, batch_size=32)
     print("\nTesting result:")
     model.evaluate(testX, testY, verbose=2)
-    print()
-    model.summary()
+
+    model.save("model")
 
     return model
 
+def loadModel() -> ST:
+    custom_objects = {"custom_standardization": custom_standardization}
+    with tf.keras.utils.custom_object_scope(custom_objects):
+        loaded = tf.keras.models.load_model(SAVED_MODEL_DIR)
+        return loaded
 
 def main():
-    model = getTrainedModel()
+    model = loadModel()
+
+    # the model outputs a negative value for a negative sentiment, and vice versa
     with open("sample.txt", "r", encoding="utf-8") as f:
         review = f.read()
-        res = model.predict([review])
-        print(f"Concluded sentiment: {'positive' if res == 1 else 'negative'}")
+        [[res]] = model.predict([review])
+        print(res)
+        print(f"Concluded sentiment: {'negative' if res < 0 else ('positive' if res > 0 else 'neutral')}")
 
 if __name__ == "__main__":
     main()
