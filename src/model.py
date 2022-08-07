cb234dbb5d6c5c4b86bd7a7221cc63267b065773
Tim <harvard90873@gmail.com>
2022-08-07 16:03:36 +0200

    convert to csv

--- src/model.py
+++ src/model.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 
 dataDir = "data/aclImdb"
+csvDir = "data-csv"
 
 labels = {
     "pos": 1,
@@ -10,7 +11,8 @@ labels = {
 
 def readDataToDF():
     """
-    This returns a tuple, (train, test), with two pandas data frames; one contains training data while the other contains testing data
+    This returns a tuple, (train, test), with two pandas data frames;
+    one contains training data while the other contains testing data
     """
 
     dfs = {
@@ -27,9 +29,18 @@ def readDataToDF():
             for reviewTxtFname in os.listdir(currLabelDir):
                 with open(os.path.join(currLabelDir, reviewTxtFname), "r", encoding='utf-8') as f:
                     raw = f.read().strip("\n")
-                    df = df.append([[raw, labels[label]]], ignore_index=True)
+                    dfs[dataCategory] = dfs[dataCategory].append([[raw, labels[label]]], ignore_index=True)
 
     dfs["train"].columns = cols
     dfs["test"].columns = cols
 
     return (dfs["train"], dfs["test"])
+
+def main():
+    (train, test) = readDataToDF()
+    train.to_csv(os.path.join(csvDir, "train.csv"), index=False, encoding="utf8")
+    test.to_csv(os.path.join(csvDir, "test.csv"), index=False, encoding="utf8")
+
+
+if __name__ == "__main__":
+    main()
