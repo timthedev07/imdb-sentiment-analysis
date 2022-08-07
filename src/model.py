dfd2c851a31baa2f4d5ccbeff3c62e97f2713e20
Tim <harvard90873@gmail.com>
2022-08-07 15:48:33 +0200

    update deps and init model file

---
+++ src/model.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+
+dataDir = "data/aclImdb"
+
+labels = {
+    "pos": 1,
+    "neg": 0
+}
+
+def readDataToDF():
+    """
+    This returns a tuple, (train, test), with two pandas data frames; one contains training data while the other contains testing data
+    """
+
+    dfs = {
+        "train": pd.DataFrame(),
+        "test": pd.DataFrame()
+    }
+
+    cols = ["review", "sentiment"]
+
+    for dataCategory in ["train", "test"]:
+        for label in labels.keys():
+            currLabelDir = os.path.join(dataDir, dataCategory, label)
+
+            for reviewTxtFname in os.listdir(currLabelDir):
+                with open(os.path.join(currLabelDir, reviewTxtFname), "r", encoding='utf-8') as f:
+                    raw = f.read().strip("\n")
+                    df = df.append([[raw, labels[label]]], ignore_index=True)
+
+    dfs["train"].columns = cols
+    dfs["test"].columns = cols
+
+    return (dfs["train"], dfs["test"])
